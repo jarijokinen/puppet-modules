d56cc0a6ee37fb117b15f9e375cbdae957c696b6
Jari Jokinen <info@jarijokinen.com>
2013-03-29 21:29:59 +0200

    Make chkrootkit to mail reports on startup

--- chkrootkit/manifests/init.pp
+++ chkrootkit/manifests/init.pp
@@ -4,6 +4,13 @@ class chkrootkit {
   }
   file { "/etc/chkrootkit.conf":
     ensure => "present",
-    content => template("chkrootkit/conf.erb")
+    content => template("chkrootkit/conf.erb"),
+    require => Package["chkrootkit"]
+  }
+  file { "/etc/cron.daily/chkrootkit":
+    ensure => "present",
+    content => template("chkrootkit/cron.daily.erb"),
+    require => Package["chkrootkit"],
+    mode => "0755"
   }
 }
