f11e368d7550fbde71a8be9339761e2ab6305227
Jari Jokinen <info@jarijokinen.com>
2013-05-13 14:07:36 +0300

    Add iceweasel default preferences and PaX support

--- iceweasel/manifests/init.pp
+++ iceweasel/manifests/init.pp
@@ -2,7 +2,10 @@ class iceweasel inherits iceweasel::params {
   package { "iceweasel":
     ensure  => installed,
     require => File["/etc/apt/sources.list.d/iceweasel.list"],
-    notify  => Exec["iceweasel-pax"]
+    notify  => [ 
+      Exec["iceweasel-pax"],
+      Exec["iceweasel-plugin-pax"]
+    ]
   }
   package { "pkg-mozilla-archive-keyring": 
     ensure => installed,
@@ -14,6 +17,11 @@ class iceweasel inherits iceweasel::params {
     require => Package["pkg-mozilla-archive-keyring"],
     notify  => Exec["apt-update"]
   }
+  file { "/etc/iceweasel/pref/iceweasel.js":
+    ensure  => present,
+    content => template("iceweasel/pref.js.erb"),
+    require => Package["iceweasel"]
+  }
   exec { "iceweasel-add-archive-key":
     refreshonly => true,
     command     => "/usr/bin/gpg --check-sigs --fingerprint --keyring /etc/apt/trusted.gpg.d/pkg-mozilla-archive-keyring.gpg --keyring /usr/share/keyrings/debian-keyring.gpg pkg-mozilla-maintainers"
@@ -21,6 +29,11 @@ class iceweasel inherits iceweasel::params {
   exec { "iceweasel-pax":
     refreshonly => true,
     command     => "/sbin/paxctl -crm /usr/lib/iceweasel/iceweasel",
+    onlyif      => "/usr/bin/test -f /sbin/paxctl"
+  }
+  exec { "iceweasel-plugin-pax":
+    refreshonly => true,
+    command     => "/sbin/paxctl -crm /usr/lib/xulrunner-*/plugin-container",
     onlyif      => "/usr/bin/test -f /sbin/paxctl"
   }
 }
