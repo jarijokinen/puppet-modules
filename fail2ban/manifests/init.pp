ad6fc31e02ea5025f769dd2f272061e069a8c512
Jari Jokinen <info@jarijokinen.com>
2013-06-11 10:08:15 +0300

    Add missing ssh_port value for fail2ban

--- fail2ban/manifests/init.pp
+++ fail2ban/manifests/init.pp
@@ -1,6 +1,9 @@
 class fail2ban(
   $jails = $fail2ban::params::jails
 ) inherits fail2ban::params {
+  if $jails.include('ssh') {
+    $ssh_port = hiera('ssh::port', 22)
+  }
   package { "fail2ban": 
     ensure => installed
   }
