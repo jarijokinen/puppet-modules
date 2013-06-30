9af8e0128f16871e760f98c73af060be5fc9bf60
Jari Jokinen <info@jarijokinen.com>
2013-06-30 12:34:28 +0300

    Add Spotify key

--- spotify/manifests/init.pp
+++ spotify/manifests/init.pp
@@ -1,11 +1,22 @@
 class spotify inherits spotify::params {
   package { "spotify-client":
     ensure  => installed,
-    require => File["/etc/apt/sources.list.d/spotify.list"]
+    require => File["/etc/apt/sources.list.d/spotify.list"],
+    before  => [
+      Exec["spotify-add-key"],
+      Exec["apt-update"]
+    ]
   }
   file { "/etc/apt/sources.list.d/spotify.list":
     ensure  => present,
     content => template("spotify/sources.list.erb"),
-    notify  => Exec["apt-update"]
+    notify  => [
+      Exec["apt-update"], 
+      Exec["spotify-add-key"]
+    ]
+  }
+  exec { "spotify-add-key":
+    command     => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59",
+    refreshonly => true
   }
 }
